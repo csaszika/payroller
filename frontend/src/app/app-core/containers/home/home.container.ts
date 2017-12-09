8bb3f30adf7e2632240bfde7b4158bba5f5e4bea
csaszi <csaszika4@gmail.com>
2017-12-09 16:49:07 +0100

    clarifying tasks

--- frontend/src/app/app-core/containers/home/home.container.ts
+++ frontend/src/app/app-core/containers/home/home.container.ts
@@ -10,10 +10,6 @@ import * as fromRoot from '../../reducers';
 })
 export class HomeContainerComponent implements OnInit {
 
-  /**
-   * I think this component could be the first page
-   * after login and it should contain a nice picture at the center
-   */
   constructor(private store: Store<fromRoot.AppState>) { }
 
   /**
@@ -21,16 +17,18 @@ export class HomeContainerComponent implements OnInit {
    * todo: add a jpg to this component, kind of welcome picture
    * todo 2: logout button into sidebar
    * todo 3: css fix in sidebar and header
-   * todo 4: for get to know ngrx action lifecycle create a container for FAQ menu point (you can find the json in assets)
+   * todo 4: for get to know ngrx action lifecycle create a container for FAQ menu point (you can find the mock json in assets)
    *
    * help for todo 4:
-   * 1. create 2 actions
+   * 0. create your own module for FAQ (with routing -> like in productSheet module)
+   * 1. create ngrx actions (get, loaded, add new faq, delete faq)
    * 2. create an effect to get faq data from json(your effect should be added to module)
-   * 3. create your own reducer and State
+   * 3. create your own reducer and State (FaqState)
    * 4. add selectors
    * 5. create container and select faq data from store
    * 6. add your container to routing
-   * 7. check it and pr :) todo: have fun and ask if there is a ? sign
+   * 7. check it and pr :) todo: have fun and ask if there is a ? sign in your head
+   * 8. in the end I would like to see a menu point that shows me some faq-s with answers and I can add a new one (delete is bonus)
    */
   ngOnInit() {
   }
