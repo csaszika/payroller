dc298e4e62bb7113fd47274d0834f3f9741a3c98
csaszi <csaszika4@gmail.com>
2017-12-17 18:18:39 +0100

    alternative for action handling

--- frontend/src/app/product-sheet/actions/product-sheets/product-sheets.actions.ts
+++ frontend/src/app/product-sheet/actions/product-sheets/product-sheets.actions.ts
@@ -1,18 +1,32 @@
 import {Action} from '@ngrx/store';
 import {GET_PRODUCT_SHEETS, PRODUCT_SHEETS_LOADED} from './product-sheets-action.types';
 import {ProductSheet} from '../../types/product-sheet.types';
+import {ProductSheetState} from '../../reducers/product-sheet/product-sheet.reducer';
 
-export class GetProductSheetsAction implements Action {
+export class GetProductSheetsAction implements ActionWithExecute {
   readonly type = GET_PRODUCT_SHEETS;
 
-  constructor() {}
+  constructor(public payload: any = null) {}
+  execute(state: ProductSheetState, action: Actions): ProductSheetState {
+    return state;
+  }
 }
 
-export class ProductSheetsLoadedAction implements Action {
+export class ProductSheetsLoadedAction implements ActionWithExecute {
   readonly type = PRODUCT_SHEETS_LOADED;
 
   constructor(public payload: ProductSheet[]) {}
+  execute(state: ProductSheetState, action: Actions): ProductSheetState{
+    return <ProductSheetState>{
+      ...state,
+      productSheets: action.payload as ProductSheet[]
+    };
+  }
 }
 
 export type Actions = GetProductSheetsAction
   | ProductSheetsLoadedAction;
+
+export interface ActionWithExecute extends Action{
+  execute(state: ProductSheetState, action: Actions): ProductSheetState;
+}
