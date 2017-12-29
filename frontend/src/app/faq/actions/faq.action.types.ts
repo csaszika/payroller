6c25e9dbf138a2b29154b16ca1eb200b13c2a1b2
csaszi <csaszika4@gmail.com>
2017-12-29 22:26:11 +0100

    +

--- frontend/src/app/faq/actions/faq.action.types.ts
+++ frontend/src/app/faq/actions/faq.action.types.ts
@@ -1,6 +1,6 @@
 export const GET_FAQS = 'GET_FAQS';
 export const FAQS_LOADED = 'FAQS_LOADED';
-export const PUT_FAQ = 'PUT_FAQ';
+export const POST_FAQ = 'POST_FAQ';
 export const ADD_FAQ = 'ADD_FAQ';
 export const DELETE_FAQ = 'DELETE_FAQ';
 export const REMOVE_FAQ = 'REMOVE_FAQ';
