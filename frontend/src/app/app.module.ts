92ea4069fbcfd130054f2dc62687a5bf9965ff42
PanniM <medgyesy.anna@gmail.com>
2018-01-13 22:10:21 +0100

    cicoma

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,22 @@
 import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
-import {HomeContainerComponent} from './app-core/containers/home/home.container';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {DataTableModule, SidebarModule} from 'primeng/primeng';
+import {EffectsModule} from '@ngrx/effects';
+import {FaqModule} from './faq/faq.module';
 import {HeaderComponent} from './app-core/components/header/header.component';
+import {HomeContainerComponent} from './app-core/containers/home/home.container';
+import {HttpClientModule} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import {initialState, reducers} from './app-core/reducers';
+import {MenuItemEffects} from './app-core/effects/menu-item.effects';
 import {
   MenuSideBarContainerComponent,
 } from './app-core/containers/menu-side-bar/menu-side-bar.container';
-import {StoreModule} from '@ngrx/store';
-import {initialState, reducers} from './app-core/reducers';
-import {DataTableModule, SidebarModule} from 'primeng/primeng';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
-import {EffectsModule} from '@ngrx/effects';
-import {MenuItemEffects} from './app-core/effects/menu-item.effects';
-import {HttpClientModule} from '@angular/common/http';
 import {ProductSheetModule} from './product-sheet/product-sheet.module';
-import {FaqModule} from './faq/faq.module';
-
-import { HttpClient } from '@angular/common/http';
+import {StoreModule} from '@ngrx/store';
 import {TranslateModule, TranslateLoader, TranslateService} from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
